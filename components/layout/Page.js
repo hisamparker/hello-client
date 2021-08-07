8fa41411a9c96788d41a8f7da2f42285bb05e3ca
sam parker <hisamparker@gmail.com>
2021-08-07 13:25:14 +0200

    weird mergy stuff

---
+++ components/layout/Page.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import GlobalStyles from '../styles/GlobalStyles';
+import Header from './Header';
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+const Page = ({ children }) => (
+  <div>
+    <GlobalStyles />
+    <Header />
+    <InnerStyles>{children}</InnerStyles>
+  </div>
+);
+
+Page.propTypes = {
+  //  for children, proptype is an array of nodes
+  children: PropTypes.any,
+};
+
+export default Page;
