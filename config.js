4c5489ca18e567188ec6dc64c89175e5d2ad2b8b
sam parker <hisamparker@gmail.com>
2021-08-29 20:05:29 +0200

    update config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 // here's how our graphqlqueries know where to look!!!!!
-export const endpoint = `http://localhost:3000/api/graphql`;
+export const endpoint = `https://hello-server-p28z7.ondigitalocean.app/api/graphql`;
 export const prodEndpoint = `https://hello-server-p28z7.ondigitalocean.app/api/graphql`;
 // here's where we decide items per page not great to hardcode this on the page itself, so putting it here makes it easier to change
 export const perPage = 2;
